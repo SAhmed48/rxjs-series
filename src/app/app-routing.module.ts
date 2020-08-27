34bf150cef5070cf2edacebef59d52b6cf5c0b98
Salman Ahmed <salmanahmed4858@gmail.com>
2020-08-28 00:54:30 +0500

    Setup project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', loadChildren: () => import('../app/rxjs-series/rxjs-series.module').then(m => m.RxjsSeriesModule)}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RxjsSeriesModule } from './rxjs-series/rxjs-series.module';
+import { RxjsSeriesRoutingModule } from './rxjs-series/rxjs-series-routing.module';
+import { SharedModuleModule } from './shared-module/shared-module.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+  BrowserModule,
+    AppRoutingModule,
+    SharedModuleModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    RxjsSeriesRoutingModule,
+    RxjsSeriesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
