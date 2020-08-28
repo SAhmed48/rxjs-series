6c23aeedf7d845f6de8b24befbe7cf952b7a91b7
Salman Ahmed <salmanahmed4858@gmail.com>
2020-08-29 01:35:25 +0500

    Add bootstrap and jquery.

--- src/app/rxjs-series/rxjs-series-routing.module.ts
+++ src/app/rxjs-series/rxjs-series-routing.module.ts
@@ -2,16 +2,21 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { PromiseComponent } from './promise/promise.component';
 import { ObservableComponent } from './observable/observable.component';
+import { ListObsrvbleComponent } from './observable/list-obsrvble/list-obsrvble.component';
+import { FromEventComponent } from './observable/from-event/from-event.component';
+
 
 const routes: Routes = [
   { path: 'promise', component: PromiseComponent },
-  { path: 'observable', component: ObservableComponent },
+  { path: 'observable', component: ObservableComponent,
+    children: [
+      { path: '', component: ListObsrvbleComponent },
+      { path: 'from-event', component: FromEventComponent }]
+  },
 ];
 
 @NgModule({
-  imports: [
-  RouterModule.forChild(routes),
-  ],
+  imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
 export class RxjsSeriesRoutingModule { }
--- src/app/rxjs-series/rxjs-series.module.ts
+++ src/app/rxjs-series/rxjs-series.module.ts
@@ -6,9 +6,11 @@ import { RxjsSeriesRoutingModule } from './rxjs-series-routing.module';
 import { PromiseComponent } from './promise/promise.component';
 import { SharedModuleModule } from '../shared-module/shared-module.module';
 import { ObservableComponent } from './observable/observable.component';
+import { ListObsrvbleComponent } from './observable/list-obsrvble/list-obsrvble.component';
+import { FromEventComponent } from './observable/from-event/from-event.component';
 
 @NgModule({
-  declarations: [PromiseComponent, ObservableComponent],
+  declarations: [PromiseComponent, ObservableComponent, ListObsrvbleComponent, FromEventComponent],
   imports: [
   CommonModule,
     MatToolbarModule,
