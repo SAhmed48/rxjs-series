9a4521033fd9b6278d33f43f1366bcc1028952b4
Salman Ahmed <salmanahmed4858@gmail.com>
2020-08-30 19:12:54 +0500

    tap operator for logging.

--- src/app/rxjs-series/observable/tap/tap.component.ts
+++ src/app/rxjs-series/observable/tap/tap.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { interval, Subscription } from 'rxjs';
+import { map, tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-tap',
@@ -7,9 +9,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TapComponent implements OnInit {
 
+  /**
+   * Use cases of Tap Operator.
+   * Mostly used on api calls response using Pipe.
+   * console.log(Data)
+   * console.log(reponse)
+   * Change property Value
+   * Emit value in observable
+   * Perform any other action
+   * Does not effect any action
+   */
+
+  streamSubscription: Subscription;
+  dataPrint: string[] = [];
+  loggingList: string[] = [];
+  users = ['John', 'Andrew', 'Haier', 'haaris', 'Client', 'Maria', 'Row'];
+
   constructor() { }
 
   ngOnInit(): void {
+
+    const getUserStream = interval(1500);
+
+    this.streamSubscription = getUserStream.pipe(
+      tap(res => {
+        this.loggingList.push(`Before Response Logging => ${res}`);
+        if(res == this.users.length){
+          this.streamSubscription.unsubscribe();
+        }
+      }),
+      map(res => this.users[res]),
+      tap(res => {
+        this.loggingList.push(`After Subscribe Logging => ${res}`);
+      }))
+      .subscribe(res => {
+      this.dataPrint.push(res);
+    })
   }
 
 }
