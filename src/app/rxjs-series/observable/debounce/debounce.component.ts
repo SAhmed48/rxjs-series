0985bebf7031b019c0de2c55f3f4bc73a1eaf646
Salman Ahmed <salmanahmed4858@gmail.com>
2020-09-04 00:11:13 +0500

    Add debounceTime and distinctUntilChanged example.

--- src/app/rxjs-series/observable/debounce/debounce.component.ts
+++ src/app/rxjs-series/observable/debounce/debounce.component.ts
@@ -1,15 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
+import { fromEvent } from 'rxjs';
+import { map, debounceTime, distinctUntilChanged } from 'rxjs/operators';
 
 @Component({
   selector: 'app-debounce',
   templateUrl: './debounce.component.html',
   styleUrls: ['./debounce.component.scss']
 })
-export class DebounceComponent implements OnInit {
+export class DebounceComponent implements OnInit, AfterViewInit {
+
+  @ViewChild('distinctUntilInput') distinctUntilInput: ElementRef;
+  @ViewChild('debounceInput') debounceInput: ElementRef;
+
+  searchData: string = '';
+  searchData1: string = '';
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  ngAfterViewInit(): void {
+    const debounceEvent = fromEvent<any>(this.debounceInput.nativeElement, 'keyup').pipe(
+      map(event => event.target.value ),
+      debounceTime(500)
+    );
+    debounceEvent.subscribe(res => {
+      this.searchData = res;
+    });
+
+
+    //Ex -02
+    const distinctUntil = fromEvent<any>(this.distinctUntilInput.nativeElement, 'keyup').pipe(
+      map(event => event.target.value ),
+      debounceTime(500),
+      distinctUntilChanged()
+    );
+    distinctUntil.subscribe(res => {
+      this.searchData1 = res;
+    });
   }
 
 }
