a28810aae87605b0f314e5ee73a67e78eef79b66
Salman Ahmed <salmanahmed4858@gmail.com>
2020-09-05 19:26:04 +0500

    Add replay subject example.

--- src/app/rxjs-series/observable/replay-subject/replay-subject.component.ts
+++ src/app/rxjs-series/observable/replay-subject/replay-subject.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ReplaySubject, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-replay-subject',
@@ -7,9 +8,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReplaySubjectComponent implements OnInit {
 
+  _replaySubject = new ReplaySubject<string>(3);
+
+  _userList = [];
+  _userList1 = [];
+
+  subscribed = false;
+
+  // Subscription
+  _subscription : Subscription;
+
   constructor() { }
 
   ngOnInit(): void {
+    this._replaySubject.subscribe(res => {
+       this._userList.push(res);
+    });
+
+  }
+
+  addText(value) {
+    this._replaySubject.next(value);
+  }
+
+  subscribedUser(){
+    if(this.subscribed){
+      this._subscription.unsubscribe();
+    } else {
+      this._subscription = this._replaySubject.subscribe(res => {
+        this._userList1.push(res);
+      });
+    }
+    this.subscribed = !this.subscribed;
   }
 
 }
